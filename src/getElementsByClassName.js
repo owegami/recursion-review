3d1a4b2334926b37e603a72d2d779a4341f3f9ef
ClaySamuelC <claysamuelc@gmail.com>
2020-08-17 18:14:43 -0700

    assignment complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+
+  //declare result array
+  //check the classname of the node vs. the className argument (index of)
+  //if classname of node and className are the same, push that node into the array result
+
+  //check if there are childnodes
+  var searchNodes = function(node) {
+    if (node.classList) {
+      if (node.classList.contains(className)) {
+        results.push(node);
+      }
+      if (node.childNodes) {
+        for (var i = 0; i < node.childNodes.length; i++) {
+          searchNodes(node.childNodes[i]);
+        }
+      }
+    }
+  };
+
+  searchNodes(document.body);
+
+  return results;
 };
